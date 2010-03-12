143c3bf7876046af231a411c5cdccc383757fc88
isiri Technologies <isiri@isiri-desktop.(none)>
2010-03-12 14:39:41 +0530

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,31 +2,16 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
   end
 
 
   def show
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
 
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
   def edit
@@ -58,11 +43,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(users_url) 
   end
 
 end
