18f5c694e5c7f6db5a92c17f21c76f4da9ba23e6
isiri Technologies <isiri@isiri-desktop.(none)>
2010-03-12 14:37:52 +0530

    users-new

--- db/migrate/20100312061514_create_users.rb
+++ db/migrate/20100312061514_create_users.rb
@@ -5,10 +5,10 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :password
       t.string :password_confirmation
-      t.string :crypted_password
-      t.string :password_salt
-      t.string :persistence_token
-      t.integer :login_count, :default => 0
+      t.string :crypted_password, :null => false
+      t.string :password_salt, :null => false
+      t.string :persistence_token, :null => false
+      t.integer :login_count, :default => 0, :null => false
       t.datetime :last_request_at
       t.datetime :last_login_at
       t.datetime :current_login_at
