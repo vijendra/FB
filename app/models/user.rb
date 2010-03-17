134970564a96680efc9d4635153ddc6d96d93ab0
isiri Technologies <isiri@isiri-desktop.(none)>
2010-03-17 13:23:45 +0530

    role_requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   has_many :registration_forms
+  has_and_belongs_to_many :roles
   attr_accessible :username, :email, :password,:password_confirmation
 
   acts_as_authentic do |c|
     c.login_field = :username
   end
-
+  
+  def user_type
+     @_list ||= self.roles.collect(&:name)
+  end
 end
--- db/migrate/20100312061514_create_users.rb
+++ db/migrate/20100312061514_create_users.rb
@@ -14,12 +14,44 @@ class CreateUsers < ActiveRecord::Migration
       t.datetime :current_login_at
       t.string :last_login_ip
       t.string :current_login_ip
-	  t.integer :hospital_id
       t.timestamps
     end
   
     add_index :users, :persistence_token
     add_index :users, :last_request_at
+
+    create_table :roles do |t|
+      t.column :name, :string
+    end
+    
+    # generate the join table
+    create_table :roles_users, :id => false do |t|
+      t.column :role_id, :integer
+      t.column :user_id, :integer
+    end
+  
+    
+    # Create admin role 
+    admin = Role.create(:name => 'Admin1')
+    user1 = User.create do |u|
+      u.username = 'admin1'
+      u.password = u.password_confirmation = 'admin1'
+      u.email = 'admin1@example.com'
+    end
+    user2 = User.create do |u|
+      u.username = 'admin2'
+      u.password = u.password_confirmation = 'admin2'
+      u.email = 'admin2@example.com'
+    end
+    user3 = User.create do |u|
+      u.username = 'admin3'
+      u.password = u.password_confirmation = 'admin3'
+      u.email = 'admin3@example.com'
+    end
+    user1.roles << admin
+    user2.roles << admin
+    user3.roles << admin
+    add_index :users, :username, :unique => true
   end
 
   def self.down
